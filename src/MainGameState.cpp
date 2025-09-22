c28747b12356c383684f641197dfce8bfde23a3e
Jorge ros <jrg94@gcloud.ua.es>
2025-09-22 17:21:12 +0200

    Creacion del metodo Handleinput

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -7,15 +7,26 @@ extern "C" {
 
 MainGameState::MainGameState()
 {
+
 }
 
 void MainGameState::init()
 {
+    jugador.x =  200.0f;
+    jugador.y = 200.0f;
 
 }
 
 void MainGameState::handleInput()
 {
+    /**
+        @brief si se presiona el espacio la velocidad vertical del jugador sube(hacia arriba) en 300.0f
+    */    
+
+    if (IsKeyPressed(KEY_SPACE))
+    {
+        jugador.vy = -300.0f;  
+    }
 
 }
 
