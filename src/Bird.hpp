2f2b133a749ddde49efc5b2d7de699d84eb63161
Jorge ros <jrg94@gcloud.ua.es>
2025-09-22 17:12:26 +0200

    Creación struct Bird con docu

---
+++ src/Bird.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+
+/**
+    *@struct bird
+    *@brief Representación del pajaro 
+*/
+
+struct Bird {
+
+    /**
+     * @brief Posicion horizontal 
+     */
+    float x;   
+    /**
+     * @brief Posición Vertical 
+     */
+    float y;  
+    /**
+     * @brief Velocidad vertical 
+     */
+    float vy;  
+};
