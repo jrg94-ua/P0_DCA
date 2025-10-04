d0730945e66688d563f7e02125a3ef522135e40e
Jorge ros <jrg94@gcloud.ua.es>
2025-10-04 13:49:06 +0200

    include

--- src/MainGameState.hpp
+++ src/MainGameState.hpp
@@ -1,23 +1,32 @@
 #pragma once
 #include <GameState.hpp>
 #include <Bird.hpp>
+#include <PipePair.hpp>
+#include <deque>
 
 class MainGameState : public GameState
 {
-    public:
-        MainGameState();
-        ~MainGameState() = default;
+public:
+    MainGameState();
+    ~MainGameState() = default;
 
-        void init() override;
-        void handleInput() override;
-        void update(float deltaTime) override;
-        void render() override;
+    void init() override;
+    void handleInput() override;
+    void update(float deltaTime) override;
+    void render() override;
 
-        void pause(){};
-        void resume(){};
+    void pause() {};
+    void resume() {};
 
-    
-    private:
-        char entered_key;
-        Bird jugador;
+private:
+    char entered_key;
+    Bird jugador;
+    std::deque<PipePair> pipes;
+
+    float spawnTimer = 0.0f;
+    float spawnEvery = 2.0f;
+
+    static constexpr float PIPE_W = 32.0f;
+    static constexpr float PIPE_H = 320.0f;
+    static constexpr float PIPE_SPEED = 100.0f;
 };
